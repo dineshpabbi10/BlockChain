ba8451a928faf87998c64b462a1d5608a2417497
unknown <dineshpabbi10@gmail.com>
2020-02-23 13:39:35 -0800

    Practice sol complete

--- src/contracts/practice.sol
+++ src/contracts/practice.sol
@@ -13,7 +13,7 @@ contract practice{
     struct item{
         uint256 id;
         string itemName;
-        address owner;
+        address payable owner;
         bool isSold;
         uint price;
     }
@@ -39,5 +39,21 @@ contract practice{
     function createItem(string memory name,uint256 price) public{
         require(bytes(name).length > 0,"Item name Cannot be Empty");
         require(price > 0,"Price should be greater than 0");
+        itemId++;
+        items[itemId] = item(itemId,name,msg.sender,false,price);
+        emit createItemEvent(itemId,name,msg.sender,false,price);
+    }
 
-  
+    function buyItem(uint256 id) public payable{
+        address payable buyer = msg.sender;
+        item memory _item= items[id];
+        require(id>0 && id < itemId,"Item not available");
+        require(buyer.balance > _item.price && msg.value > _item.price,"Either Balance or value specified is less than item price");
+        require(buyer != _item.owner,"Owner of item cannot be buyer himself");
+        require(_item.isSold == false,"Item already sold");
+        items[id].isSold = true;
+        _item.owner.transfer(msg.value);
+        items[id].owner = buyer;
+        emit createItemEvent(items[id].id,items[id].itemName,items[id].owner,items[id].isSold,items[id].price);
+    }
+}
